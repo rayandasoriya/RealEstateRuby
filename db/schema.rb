baf7923f9fef6e5280451ae2c9fe3ead3e45d9fa
Rayan Dasoriya <dasoriyarayan@gmail.com>
2018-09-28 18:04:19 -0400

    URL error fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_014355) do
+ActiveRecord::Schema.define(version: 2018_09_28_213231) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_014355) do
     t.boolean "is_hunter"
     t.boolean "is_realtor"
     t.integer "contact_method"
+    t.integer "company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
