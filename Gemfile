71dec139faa44abd3fd4a8f524a7b3b5a1ec75bb
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-09-27 21:09:21 -0400

    devise setup

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
+# Use devise
+gem 'devise'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Ensure you have defined default url options in your environments files. Here
+  #    is an example of default_url_options appropriate for a development environment
+  #    in config/environments/development.rb:
+
+  #      config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #    In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
