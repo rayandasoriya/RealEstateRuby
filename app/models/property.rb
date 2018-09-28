775f80392cfd5a6d3f50fdf06995677af00ac4b1
Juhi Madhwani <juhimadhwani1296@gmail.com>
2018-09-28 19:12:22 -0400

    Properties Modified

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
-  validates_presence_of :address, :owner, :price, :size, :basement, :floors, :style
-  enum style: [:townhouse, :apartment]
+  validates_presence_of :address, :owner, :price, :size, :basement, :floors, :styles
+  enum styles: [:townhouse, :apartment]
   enum basement: [:yes, :no]
   enum floors: [:"1", :"2", :"3", :"4", :"5+"]
   has_many_attached :images
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_215131) do
+ActiveRecord::Schema.define(version: 2018_09_28_215129) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
