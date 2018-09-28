fb3a50537bc50148a63cfc9ddea01176b691e3ed
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-09-28 19:13:53 -0400

    create companies view

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,6 @@ class Company < ApplicationRecord
   validates_presence_of :name, :address, :website
   validates_length_of :year_founded, is: 4,  message: "Year must be in YYYY"
   enum size: [:"0-50", :"51-200", :"201-500", :"501-1000", :"1000+"]
+  enum revenue: [:"Below 1 Million", :"1 to 10 Million", :"10 to 100 Million", :"More than 100 Million"]
   validates :website, :format => URI::regexp(%w(http https))
 end
