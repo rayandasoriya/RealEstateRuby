67915d83faec3026238f8dd57a33c8d0e4ca6315
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-09-29 01:29:17 -0400

    Company List Page | Company -> index.html.erb

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -69,6 +69,6 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.fetch(:company, {})
+      params.fetch(:company, {}).permit(:name, :website, :address, :year_founded, :size, :revenue, :synopsis)
     end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -4,5 +4,5 @@ class Company < ApplicationRecord
   validates_length_of :year_founded, is: 4,  message: "Year must be in YYYY"
   enum size: [:"0-50", :"51-200", :"201-500", :"501-1000", :"1000+"]
   enum revenue: [:"Below 1 Million", :"1 to 10 Million", :"10 to 100 Million", :"More than 100 Million"]
-  validates :website, :format => URI::regexp(%w(http https))
+  validates :website, url: true
 end
