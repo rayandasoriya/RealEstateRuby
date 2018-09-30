9446c3e6444b1b9ed3ba19f8b801919d312c81b4
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-09-30 02:14:39 -0400

    Precompile assets to false for heroku

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module HouseRentalApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
