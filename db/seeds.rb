c1bab932d9cd807c1a0bf7a555686e7941096ac1
Rayan Dasoriya <dasoriyarayan@gmail.com>
2018-09-29 20:25:43 -0400

    Admin created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([{first_name: 'Admin', last_name: 'Admin', email: 'admin@ats.com',
+               password: 'password', is_hunter: 0, is_realtor: 0, is_admin: 1,
+               phone: '0000000000'}])
