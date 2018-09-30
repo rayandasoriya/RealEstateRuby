ec9c1054e7ac088ff7591992ddbfe032244c556c
Rayan Dasoriya <dasoriyarayan@gmail.com>
2018-09-29 21:23:18 -0400

    Add a company name for realtor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   private
   def sign_up_params
-    params.require(:user).permit(:id, :first_name, :last_name, :phone, :email, :password, :is_recruiter, :is_applicant)
+    params.require(:user).permit(:id, :first_name, :last_name, :phone, :email, :password, :is_recruiter, :is_applicant,:company_id)
   end
 
   def account_update_params
-    params.require(:user).permit(:id, :first_name, :last_name,:phone, :name, :email, :password, :current_password, :is_recruiter, :is_applicant)
+    params.require(:user).permit(:id, :first_name, :last_name,:phone, :name, :email, :password, :current_password, :is_recruiter, :is_applicant, :company_id)
   end
 
   def user_params
-    params.require(:user).permit(:id, :first_name, :last_name, :phone, :email, :password, :is_hunter, :is_realtor)
+    params.require(:user).permit(:id, :first_name, :last_name, :phone, :email, :password, :is_hunter, :is_realtor,:company_id)
   end
 end
