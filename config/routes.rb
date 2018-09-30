368b6a92fc228fa4cdf7c368f8ff7240d50877e3
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-09-29 20:25:24 -0400

    Hunter Realtor Pages for Admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :properties
   resources :companies
   devise_for :users
+  match '/users', to: 'users#index', via: 'get'
+  match '/hunters', to: 'users#hunter', via: 'get'
+  match '/realtors', to: 'users#realtor', via: 'get'
   root to: "house_rental_app#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
