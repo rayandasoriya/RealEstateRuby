ace85b12f4656ceea5ca6906169323d517ca0966
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-10-01 11:57:05 -0400

    Fix Potential Buyers Migration

--- db/migrate/20180930013059_potential_buyer.rb
+++
@@ -1,4 +0,0 @@
-class PotentialBuyer < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_222218) do
+ActiveRecord::Schema.define(version: 2018_10_01_155412) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -97,9 +97,9 @@ ActiveRecord::Schema.define(version: 2018_09_30_222218) do
     t.string "phone"
     t.boolean "is_admin"
     t.integer "contact_method"
-    t.integer "company_id"
     t.boolean "is_hunter"
     t.boolean "is_realtor"
+    t.integer "company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
