6ad0cb1b9dc8b3b1bf953ebee4aa91c8374c0f0d
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-10-01 22:34:24 -0400

    Used Faker to Seed Data into the database

--- Gemfile
+++ Gemfile
@@ -77,3 +77,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Gem to seed the DB
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
-#
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!([{
@@ -40,3 +42,29 @@ User.create!([{
   phone: '1800-MY-HUNTER',
   contact_method: 'text'
 }])
+
+200.times do
+  User.create(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email,
+    password: 'password', 
+    is_hunter: Faker::Boolean.boolean, 
+    is_realtor: Faker::Boolean.boolean, 
+    is_admin: 0,
+    phone: Faker::PhoneNumber.phone_number,
+    contact_method: 'email'
+  )
+end
+
+30.times do
+  Company.create(
+    name: Faker::Company.name,
+    website: Faker::Internet.url,
+    address: Faker::Address.full_address,
+    size: Faker::Number.between(0, 4),
+    year_founded: Faker::Number.between(1900, 2018),
+    revenue: Faker::Number.between(0, 3),
+    synopsis: Faker::Dota.quote
+  )
+end
