473789c39a071c2b163b8bc83f951ab508d8a21a
Rayan Dasoriya <dasoriyarayan@gmail.com>
2018-10-10 09:11:22 -0400

    Destroy dependency added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :company
+  has_many :queries, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
