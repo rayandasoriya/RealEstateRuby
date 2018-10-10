87fa817e014c97732131040653bc7540e9ebf9c4
Harsh Agrawal <hrsh.agrwl1@gmail.com>
2018-10-10 17:36:47 -0400

    Minor Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:google]
 
-  validates_presence_of :email, :is_hunter, :is_realtor, :first_name
+  validates_presence_of :email, :first_name
   enum contact_method: [:email, :text, :call]
 
   def self.from_omniauth(auth)
